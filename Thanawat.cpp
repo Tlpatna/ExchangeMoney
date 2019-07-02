a76dbfd63f285700495baaa9a8d1efc42fe3e045
Tlpatna <52210006+Tlpatna@users.noreply.github.com>
2019-07-02 19:46:28 +0700

    Create Thanawat.cpp

---
+++ Thanawat.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	float bahts;
+cout << "Enter your money :";
+cin >> bahts;
+cout << "Exchange bahts to dollar :" << (bahts * 0.03256) << endl;
+cout << "Exchange bahts to Euto   :" << (bahts * 0.02856);
+}
